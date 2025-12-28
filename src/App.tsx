da80b934adcb0c4ca092133909014fb26e36ecd1
hellomehedii <hellomehediii4@gmail.com>
2025-12-28 17:33:53 +0600

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,43 @@ const AppWrapper = () => {
       <RealtimeInvalidation />
       {showWhatsappButton && <WhatsappButton />}
 
-      <Suspense fallback={<div>Loading...</div>}>
+
+
+<Suspense
+  fallback={
+    <div>
+      <svg
+        width="50"   // ↓ Set your desired width
+        height="50"  // ↓ Set your desired height
+        fill="hsl(228, 97%, 42%)"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g>
+          <circle cx="12" cy="3" r="1">
+            <animate
+              id="spinner_7Z73"
+              begin="0;spinner_tKsu.end-0.5s"
+              attributeName="r"
+              calcMode="spline"
+              dur="0.6s"
+              values="1;2;1"
+              keySplines=".27,.42,.37,.99;.53,0,.61,.73"
+            />
+          </circle>
+          {/* ...rest of your circles and animations */}
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="6s"
+            values="360 12 12;0 12 12"
+            repeatCount="indefinite"
+          />
+        </g>
+      </svg>
+    </div>
+  }
+>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/auth" element={<Auth />} />
